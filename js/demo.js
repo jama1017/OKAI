77808f1b79ae0197d3def37c3125eafc2be685bc
Jiaju Ma <majiaju@hotmail.com>
2018-07-04 17:48:51 -0400

    simple neuron graphics setup

---
+++ js/demo.js
@@ -0,0 +1,10 @@
+// var tl = new TimelineMax({repeat:-1});
+//
+// TweenMax.set([".neuron", ".input", ".output", ".axon"], {
+//  transformOrigin:'50% 50%'
+// })
+//
+// tl.from(".neuron", 1, {scale: 0, ease: Elastic.easeOut})
+//   .from(".input", 2, {scale: 0})
+//   .from(".axon", 1, {scale: 0}, "-=1")
+//   .from(".output", 1, {scale: 0, ease: Elastic.easeOut});
