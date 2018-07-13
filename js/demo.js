1a0042b2b39a0781b967a0a29719ceb8d9a17d89
Jiaju Ma <majiaju@hotmail.com>
2018-07-12 22:43:31 -0400

    Signal Transmission Animation completed with bugs fixed

--- js/demo.js
+++ js/demo.js
@@ -27,7 +27,9 @@ let axon = select('.axon');
 let outTransit = axon.cloneNode();
 neuronGroup.appendChild(outTransit);
 
-let tl = new TimelineMax({repeat:-1});
+let outSig = select('#outSig');
+
+var tl = new TimelineMax({repeat: -1});
 
 tl.set(topSignal, {transformOrigin: '110%, 110%'});
 tl.set(midSignal, {transformOrigin: '110%, 50%'});
@@ -36,7 +38,7 @@ tl.set(botSignal, {transformOrigin: '110%, 0%'});
 tl.set(cenSig2, {transformOrigin: '0%, 50%'});
 tl.set(cenSig3, {transformOrigin: '0%, 100%'});
 
-// tl.set([cenSig, cenSig2, cenSig3], {scale: 0});
+tl.set(outSig, {transformOrigin: '0%, 50%'});
 
 tl.set(outTransit, {strokeWidth: "25px"});
 
@@ -44,8 +46,6 @@ tl.staggerTo([topSignal, midSignal, botSignal], 1, {scale: 0}, 0.5)
   .staggerFromTo(transit, 1.5, {drawSVG:'0% 10%'}, {drawSVG: "90%, 100%"}, 0.5, "-=1.95")
   .staggerFromTo([cenSig, cenSig2, cenSig3], 2.5, {scale: 0}, {scale: 1}, 0.5, "-=2")
   .set([cenSig, cenSig2, cenSig3], {transformOrigin: '110% 50%'}, "-=1")
-  .to([cenSig, cenSig2, cenSig3], 2.5, {scale: 0}, "-=1")
-  .fromTo(outTransit, 2, {drawSVG: "90%, 100%"}, {drawSVG: "0% 10%"}, "-=2.5");
-
-// tl.pause();
-GSDevTools.create();
+  .to([cenSig, cenSig2, cenSig3], 2.5, {scale: 0, overwrite:"none"}, "-=1")
+  .fromTo(outTransit, 2, {drawSVG: "90%, 100%"}, {drawSVG: "0% 10%"}, "-=2.5")
+  .fromTo(outSig, 1, {scale: 0}, {scale: 1}, "-=1.5");
