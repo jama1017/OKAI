1b78f4f039c79c56863f91286bff6f9d434fc44d
Jiaju Ma <majiaju@hotmail.com>
2018-07-24 00:07:18 -0400

    input signal sequencing updated. first scene delay resolved by additional class. minor tweaks to animation

--- js/demo.js
+++ js/demo.js
@@ -88,19 +88,13 @@ let numBig5 = select('#numBig5');
 // ------------------ Animation Timelines--------------------
 // --------------------------------------------------------
 
-// //brain timeline
-// let brainTL = new TimelineMax();
-//
-// brainTL.from(face, 0.5, {scale: 0,transformOrigin: '50%, 50%'})
-//        .from(brain, 0.5, {opacity: 0, y: +100})
-
 //brain to nerve transition
 let transitionTL = new TimelineMax();
 
 transitionTL.to(brain, 1, {scale: 2,opacity: 0,y: +50,
                            transformOrigin: '50%, 50%'}, "brainZoom")
             .to(face, 1, {opacity: 0, y: +100}, "brainZoom")
-            .fromTo(nerveGroup, 1, {scale: 0,transformOrigin: '50%, 50%'},
+            .fromTo(nerveGroup, 1, {scale: 0,transformOrigin: '50%, 20%'},
                                    {scale: 1}, "brainZoom")
 
 //nerve system timeline
@@ -152,35 +146,34 @@ appearanceTL.from(neuron, 1, {scale: 0,
            .from([input1, input2], 1, {scale: 0,
                                        transformOrigin: '50%, 50%',
                                        ease: Elastic.easeOut})
-           .staggerFrom([dendrites1, dendrites2], 1, {drawSVG: '0%, 0%'}, 1)
-           .from([topSignal, botSignal, num1, num3], 1, {scale: 0,
-                                             transformOrigin: '50%, 50%',
-                                             ease: Elastic.easeOut})
 
 //top signal comming into the center neuron timeline
 let topSigInTL = new TimelineMax();
 
 topSigInTL.set(inTransit1, {strokeWidth: '28px'})
-         .add("enterCenSig1", 0.8);
+         .add("enterCenSig1", 1.8);
 
-topSigInTL.to(topSignal, 0.7, {scale: 0, transformOrigin: '110%, 110%'}, "topSignal")
-    .to(num1, 0.5, {scale: 0, opacity: 0, transformOrigin: '360%, 140%'}, "topSignal")
-    .fromTo(inTransit1, 1.5, {drawSVG:'0% 10%'}, {drawSVG: "90%, 100%"}, "topSignal")
+topSigInTL.from(dendrites1, 1, {drawSVG: '0%, 0%'}, "connection")
+          .from([topSignal, num1], 1, {scale: 0, transformOrigin: '50%, 50%', ease: Elastic.easeOut}, "connection")
+          .to(topSignal, 0.7, {scale: 0, transformOrigin: '110%, 110%'}, "topSignal")
+          .to(num1, 0.5, {scale: 0, opacity: 0, transformOrigin: '360%, 140%'}, "topSignal")
+          .fromTo(inTransit1, 1.5, {drawSVG:'0% 10%'}, {drawSVG: "90%, 100%"}, "topSignal")
 
-    .fromTo(cenSig, 1, {scale: 0}, {scale: 1}, "enterCenSig1")
-    .fromTo(num2, 1, {scale: 0, opacity: 0, transformOrigin: '-110%, -20%'}, {scale: 1,  opacity: 1}, "enterCenSig1");
+          .fromTo(cenSig, 1, {scale: 0}, {scale: 1}, "enterCenSig1")
+          .fromTo(num2, 1, {scale: 0, opacity: 0, transformOrigin: '-110%, -20%'}, {scale: 1,  opacity: 1}, "enterCenSig1");
 
 //botSignal timeline
 let botSigInTL = new TimelineMax();
 
-botSigInTL.add("enterCenSig3", 0.8);
+botSigInTL.add("enterCenSig3", 1.8);
 
-botSigInTL.to(botSignal, 1, {scale: 0, transformOrigin: '110%, 0%'}, "botSignal")
-    .to(num3, 0.7, {scale: 0,  opacity: 0, transformOrigin: '240%, -40%'}, "botSignal")
-    .fromTo(inTransit2, 1.5, {drawSVG:'0% 10%'}, {drawSVG: "90%, 100%"}, "botSignal")
+botSigInTL.from(dendrites2, 1, {drawSVG: '0%, 0%'}, "connection")
+          .from([botSignal, num3], 1, {scale: 0, transformOrigin: '50%, 50%', ease: Elastic.easeOut}, "connection")
+          .to(botSignal, 1, {scale: 0, transformOrigin: '110%, 0%'}, "botSignal")
+          .to(num3, 0.7, {scale: 0,  opacity: 0, transformOrigin: '240%, -40%'}, "botSignal")
+          .fromTo(inTransit2, 1.5, {drawSVG:'0% 10%'}, {drawSVG: "90%, 100%"}, "botSignal")
 
-    .fromTo(cenSig3, 1, {scale: 0, transformOrigin: '0%, 100%'}, {scale: 1}, "enterCenSig3")
-    //.fromTo(num4, 1, {scale: 0}, {scale: 1}, "enterCenSig3")
+          .fromTo(cenSig3, 1, {scale: 0, transformOrigin: '0%, 100%'}, {scale: 1}, "enterCenSig3")
 
 //counting number timeline
 let counting = new TimelineMax();
@@ -194,8 +187,8 @@ counting.to(num2, 0.5, {morphSVG: numBig3})
 let biasTL = new TimelineMax();
 biasTL.fromTo([neuron, cenSig, cenSig3, numBig3], 1, {y:-3}, {y:3,
                  ease:RoughEase.ease.config({
-                   strength:6,
-                   points:12,
+                   strength:14,
+                   points:10,
                    template:Linear.easeNone,
                    randomize:false
                  }) , clearProps:"y"}, "biasShake")
