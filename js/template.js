bbad701aee3b2a2a58f4f86ad5f59b16ed7e38a6
Jiaju Ma <jiaju_ma@brown.edu>
2019-01-26 15:53:12 -0500

    updated chapter 1 landing section and templates

--- js/template.js
+++ js/template.js
@@ -18,24 +18,6 @@ let toggleAnimation = function(animation, bool) {
   }
 };
 
-let keepActivation = function() {
-  toggleAnimation(neuronAnimWindow, false)
-  toggleAnimation(perceptronAnimation, false)
-  toggleAnimation(dataAnimWindow, true)
-}
-
-let keepPerceptron = function() {
-  toggleAnimation(neuronAnimWindow, false)
-  toggleAnimation(perceptronAnimation, true)
-  toggleAnimation(dataAnimWindow, false)
-}
-
-let offAnimation = function() {
-  toggleAnimation(neuronAnimWindow, false)
-  toggleAnimation(perceptronAnimation, false)
-  toggleAnimation(dataAnimWindow, false)
-}
-
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {
   myFunction()
@@ -52,7 +34,23 @@ function myFunction() {
 //--------------------Lottie Animations---------------------
 //----------------------------------------------------------
 
-//neuron animation
+//----------------opening animation
+var openingAnimWindow = select('#openingLottie'),
+  openingAnimData = {
+    wrapper: openingAnimWindow,
+    animType: 'svg',
+    loop: true,
+    prerender: true,
+    autoplay: true,
+    // path: './json/sushi_fitness.json',
+    path: './json/ch1_opening.json'
+  };
+
+var openingAnim = bodymovin.loadAnimation(openingAnimData);
+openingAnim.addEventListener('DOMLoaded', onOpeningDOMLoaded);
+openingAnim.setSpeed(1);
+
+//---------------seed animation
 var seedAnimWindow = select('#seedLottie'),
   seedAnimData = {
     wrapper: seedAnimWindow,
@@ -65,71 +63,265 @@ var seedAnimWindow = select('#seedLottie'),
   };
 
 var seedAnim = bodymovin.loadAnimation(seedAnimData);
-seedAnim.addEventListener('DOMLoaded', onNeuronDOMLoaded);
-//
-// //activation animations
-// var dataAnimWindow = select('#dataLottie'),
-//   dataAnimData = {
-//     wrapper: dataAnimWindow,
-//     animType: 'svg',
-//     loop: false,
-//     prerender: true,
-//     autoplay: false,
-//     path: './json/ch3_dataGroup.json',
-//   };
-//
-// var dataAnim = bodymovin.loadAnimation(dataAnimData);
-// dataAnim.addEventListener('DOMLoaded', ondataDOMLoaded);
+seedAnim.addEventListener('DOMLoaded', onSeedDOMLoaded);
 
-//opening animation
-var openingAnimWindow = select('#openingLottie'),
-  openingAnimData = {
-    wrapper: openingAnimWindow,
+
+//--------------robot animations
+var robotAnimWindow = select('#robotLottie'),
+  robotAnimData = {
+    wrapper: robotAnimWindow,
     animType: 'svg',
-    loop: true,
+    loop: false,
     prerender: true,
-    autoplay: true,
-    // path: './json/sushi_fitness.json',
-    path: './json/ch2_nestedPerc.json'
+    autoplay: false,
+    path: './json/ch1_robot.json',
   };
 
-var openingAnim = bodymovin.loadAnimation(openingAnimData);
-// openingAnim.addEventListener('DOMLoaded', onNeuronDOMLoaded);
+var robotAnim = bodymovin.loadAnimation(robotAnimData);
+robotAnim.addEventListener('DOMLoaded', onRobotDOMLoaded);
 
+
+//--------------agi animations
+var agiAnimWindow = select('#agiLottie'),
+  agiAnimData = {
+    wrapper: agiAnimWindow,
+    animType: 'svg',
+    loop: false,
+    prerender: true,
+    autoplay: false,
+    path: './json/ch1_agi.json',
+  };
+
+var agiAnim = bodymovin.loadAnimation(agiAnimData);
+agiAnim.addEventListener('DOMLoaded', onAgiDOMLoaded);
+
+
+//--------------data animations
+var dataAnimWindow = select('#dataLottie'),
+  dataAnimData = {
+    wrapper: dataAnimWindow,
+    animType: 'svg',
+    loop: false,
+    prerender: true,
+    autoplay: false,
+    path: './json/ch1_data.json',
+  };
+
+var dataAnim = bodymovin.loadAnimation(dataAnimData);
+dataAnim.addEventListener('DOMLoaded', onDataDOMLoaded);
+
+
+//--------------words animations
+var wordsAnimWindow = select('#wordsLottie'),
+  wordsAnimData = {
+    wrapper: wordsAnimWindow,
+    animType: 'svg',
+    loop: false,
+    prerender: true,
+    autoplay: false,
+    path: './json/ch1_words.json',
+  };
+
+var wordsAnim = bodymovin.loadAnimation(wordsAnimData);
+wordsAnim.addEventListener('DOMLoaded', onWordsDOMLoaded);
 
 //----------------------------------------------------------------------------
 //-----------------------------timelines--------------------------------------
 //----------------------------------------------------------------------------
-var neuronBeginTL = new TimelineMax();
-var neuronTL = new TimelineMax();
 
-function onNeuronDOMLoaded(e) {
-  // neuronBeginTL.to({
-  //   frame: 0
-  // }, 3, {
-  //   frame: 28,
-  //   onUpdate: function() {
-  //     neuronAnim.goToAndStop(Math.round(this.target.frame), true)
-  //   },
-  //   ease: Linear.easeNone
-  // })
-  //
-  // neuronTL.to({
-  //   frame: 28
-  // }, 3, {
-  //   frame: neuronAnim.totalFrames - 1,
-  //   onUpdate: function() {
-  //     neuronAnim.goToAndStop(Math.round(this.target.frame), true)
-  //   },
-  //   ease: Linear.easeNone
-  // })
+//----------opening TLs---------
+var openingTL = new TimelineMax();
+
+function onOpeningDOMLoaded(e) {
+  openingTL.to({
+    frame: 0
+  }, 1, {
+    frame: openingAnim.totalFrames - 1,
+    onUpdate: function() {
+      openingAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    repeat: -1,
+    yoyo: true,
+    ease: Linear.easeNone
+  })
 }
 
-// ---------------------------------------------------------
-// ------------------ Animation Timelines--------------------
-// --------------------------------------------------------
+// --------seed TLs-------------
+var seedTL = new TimelineMax();
+var carTL = new TimelineMax();
+var moreTL = new TimelineMax();
+
+function onSeedDOMLoaded(e) {
+  seedTL.to({
+    frame: 0
+  }, 3, {
+    frame: 76,
+    onUpdate: function() {
+      seedAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    ease: Linear.easeNone
+  })
+
+  carTL.to({
+    frame: 76
+  }, 3, {
+    frame: 214,
+    onUpdate: function() {
+      seedAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    ease: Linear.easeNone
+  })
+
+  moreTL.to({
+    frame: 214
+  }, 3, {
+    frame: seedAnim.totalFrames - 1,
+    onUpdate: function() {
+      seedAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    ease: Linear.easeNone
+  })
+}
+
+// --------robot TLs-------------
+var notRobotTL = new TimelineMax();
+var robotBrainTL = new TimelineMax();
+
+function onRobotDOMLoaded(e) {
+  notRobotTL.to({
+    frame: 0
+  }, 3, {
+    frame: 76,
+    onUpdate: function() {
+      robotAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    ease: Linear.easeNone
+  })
+
+  robotBrainTL.to({
+    frame: 76
+  }, 3, {
+    frame: robotAnim.totalFrames - 1,
+    onUpdate: function() {
+      robotAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    ease: Linear.easeNone
+  })
+}
+
+//--------agi TLs------------
+var aniTL = new TimelineMax();
+var agiTL = new TimelineMax();
+
+function onAgiDOMLoaded(e) {
+  aniTL.to({
+    frame: 0
+  }, 3, {
+    frame: 37,
+    onUpdate: function() {
+      agiAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    ease: Linear.easeNone
+  })
+
+  agiTL.to({
+    frame: 37
+  }, 3, {
+    frame: agiAnim.totalFrames - 1,
+    onUpdate: function() {
+      agiAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    ease: Linear.easeNone
+  })
+}
 
 
+//--------data TLs------------
+var data1TL = new TimelineMax();
+var data2TL = new TimelineMax();
+var data3TL = new TimelineMax();
+var data4TL = new TimelineMax();
+
+function onDataDOMLoaded(e) {
+  data1TL.to({
+    frame: 0
+  }, 3, {
+    frame: 76,
+    onUpdate: function() {
+      dataAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    ease: Linear.easeNone
+  })
+
+  data2TL.to({
+    frame: 76
+  }, 3, {
+    frame: 152,
+    onUpdate: function() {
+      dataAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    ease: Linear.easeNone
+  })
+
+  data3TL.to({
+    frame: 152
+  }, 3, {
+    frame: 229,
+    onUpdate: function() {
+      dataAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    ease: Linear.easeNone
+  })
+
+  data4TL.to({
+    frame: 229
+  }, 3, {
+    frame: dataAnim.totalFrames - 1,
+    onUpdate: function() {
+      dataAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    ease: Linear.easeNone
+  })
+}
+
+
+
+//--------words TLs------------
+var mlTL = new TimelineMax();
+var dlTL = new TimelineMax();
+var algoTL = new TimelineMax();
+
+function onWordsDOMLoaded(e) {
+  mlTL.to({
+    frame: 0
+  }, 3, {
+    frame: 75,
+    onUpdate: function() {
+      wordsAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    ease: Linear.easeNone
+  })
+
+  dlTL.to({
+    frame: 75
+  }, 3, {
+    frame: 142,
+    onUpdate: function() {
+      wordsAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    ease: Linear.easeNone
+  })
+
+  algoTL.to({
+    frame: 142
+  }, 3, {
+    frame: wordsAnim.totalFrames - 1,
+    onUpdate: function() {
+      wordsAnim.goToAndStop(Math.round(this.target.frame), true)
+    },
+    ease: Linear.easeNone
+  })
+}
 
 // ---------------------------------------------------------
 // ------------------ Scrolling Control--------------------
@@ -150,196 +342,201 @@ $(".scene").each(function() {
     .addTo(controller);
 });
 
-toggleAnimation(neuronAnimWindow, false);
-toggleAnimation(perceptronAnimation, false);
+
+toggleAnimation(seedAnimWindow, false);
+toggleAnimation(robotAnimWindow, false);
+toggleAnimation(agiAnimWindow, false);
 toggleAnimation(dataAnimWindow, false);
+toggleAnimation(wordsAnimWindow, false);
+
 
 //-------------------------------------------------
 //--------- Opening Animations -------------
 //-------------------------------------------------
 
-// let openingAnimScene = new ScrollMagic.Scene({
-//     triggerElement: "#openingText",
-//     duration: '100%'
-//   })
-//   .on("end", function(event) {
-//     var direction = event.scrollDirection;
-//     if (direction == "FORWARD") {
-//       toggleAnimation(neuronAnimWindow, true)
-//     } else {
-//       toggleAnimation(neuronAnimWindow, false)
-//     }
-//   })
-//   // .addIndicators({name: "opening"})
-//   .addTo(controller);
+let openingAnimScene = new ScrollMagic.Scene({
+    triggerElement: "#openingText",
+    duration: '100%'
+  })
+  .on("end", function(event) {
+    var direction = event.scrollDirection;
+    if (direction == "FORWARD") {
+      toggleAnimation(seedAnimWindow, true)
+    } else {
+      toggleAnimation(seedAnimWindow, false)
+    }
+  })
+  .addTo(controller);
 
 //-------------------------------------------------
-//--------- NEURON - BRAIN Animations -------------
+//--------- SEED Animations -------------
 //-------------------------------------------------
-// let nerveBeginScene = new ScrollMagic.Scene({
-//   triggerElement: "#text1",
-//   duration: '100%'
-// })
-//   .setTween(neuronBeginTL)
-//   .addTo(controller);
-//
-// let nerveAnimScene = new ScrollMagic.Scene({
-//     triggerElement: "#nerveText",
-//     duration: '100%'
-//   })
-//   .setTween(neuronTL)
-//   // .on("enter", function() {
-//   //   toggleAnimation(neuronAnimWindow, true)
-//   //   toggleAnimation(perceptronAnimation, false)
-//   //   toggleAnimation(dataAnimWindow, false)
-//   // })
-//   // .on("start", function(event) {
-//   //   var direction = event.scrollDirection;
-//   //   if (direction == "REVERSE") {
-//   //     toggleAnimation(openingAnimWindow, true)
-//   //     toggleAnimation(neuronAnimWindow, false)
-//   //   }
-//   // })
-//   // .setPin("#brainNerveAnimation")
-//   // .addIndicators({name: "nerveAnim"})
-//   .addTo(controller);
+let seedScene = new ScrollMagic.Scene({
+  triggerElement: "#text1",
+  duration: '100%'
+})
+  .setTween(seedTL)
+  .addTo(controller);
+
+let carScene = new ScrollMagic.Scene({
+    triggerElement: "#text2",
+    duration: '100%'
+  })
+  .setTween(carTL)
+  .addTo(controller);
+
+let moreScene = new ScrollMagic.Scene({
+    triggerElement: "#text3",
+    duration: '100%'
+  })
+  .setTween(moreTL)
+  .addTo(controller);
 
 //-------------------------------------------------
-//----------- PERCEPTRON ANIMATIONS ---------------
+//----------- Robot ANIMATIONS ---------------
 //-------------------------------------------------
-// let neuronAppearScene = new ScrollMagic.Scene({
-//     triggerElement: "#neuron1Text",
-//     duration: '100%'
-//   })
-//   .setTween(appearanceTL)
-//   .on("enter", function() {
-//     keepPerceptron()
-//   })
-//   .on("start", function(event) {
-//     var direction = event.scrollDirection;
-//     if (direction == "REVERSE") {
-//       toggleAnimation(neuronAnimWindow, true)
-//       toggleAnimation(perceptronAnimation, false)
-//     }
-//   })
-//   // .setPin("#perceptronAnimation")
-//   .addTo(controller);
-//
-// let neuronTopSigScene = new ScrollMagic.Scene({
-//     triggerElement: "#neuron2Text",
-//     duration: '100%'
-//   })
-//   .setTween(topSigInTL)
-//   .on("enter", function() {
-//     keepPerceptron()
-//   })
-//   // .setPin("#perceptronAnimation")
-//   .addTo(controller);
-//
-// let neuronBotSigScene = new ScrollMagic.Scene({
-//     triggerElement: "#neuron3Text",
-//     duration: '100%'
-//   })
-//   .setTween(botAndCountingTL)
-//   .on("enter", function() {
-//     keepPerceptron()
-//   })
-//   // .setPin("#perceptronAnimation")
-//   .addTo(controller);
-//
-// let neuronBiasScene = new ScrollMagic.Scene({
-//     triggerElement: "#neuron4Text",
-//     duration: '100%'
-//   })
-//   .setTween(biasTL)
-//   .on("enter", function() {
-//     keepPerceptron()
-//   })
-//   // .setPin("#perceptronAnimation")
-//   .addTo(controller);
-//
-// let neuronSigOutScene = new ScrollMagic.Scene({
-//     triggerElement: "#neuron5Text",
-//     duration: '100%'
-//   })
-//   .setTween(SigOutTL)
-//   .on("enter", function() {
-//     keepPerceptron()
-//   })
-//   // .setPin("#perceptronAnimation")
-//   .addTo(controller);
+let notRobotScene = new ScrollMagic.Scene({
+    triggerElement: "#text4",
+    duration: '100%'
+  })
+  .setTween(notRobotTL)
+  .on("start", function(event) {
+    var direction = event.scrollDirection;
+    if (direction == "REVERSE") {
+      toggleAnimation(seedAnimWindow, true)
+      toggleAnimation(robotAnimWindow, false)
+    } else {
+      toggleAnimation(seedAnimWindow, false)
+      toggleAnimation(robotAnimWindow, true)
+    }
+  })
+  .addTo(controller);
+
+let robotBrainScene = new ScrollMagic.Scene({
+    triggerElement: "#text5",
+    duration: '100%'
+  })
+  .setTween(robotBrainTL)
+  .addTo(controller);
 
 //-------------------------------------------------
-//----------- ACTIVATIONS ANIMATIONS ---------------
+//----------- AGI ANIMATIONS ---------------
 //-------------------------------------------------
-// let humanDetectorScene = new ScrollMagic.Scene({
-//     triggerElement: "#appanana1Text",
-//     duration: '100%'
-//   })
-//   .setTween(activationsTL)
-//   .on("enter", function() {
-//     keepActivation()
-//   })
-//   .on("start", function(event) {
-//     var direction = event.scrollDirection;
-//     if (direction == "REVERSE") {
-//       toggleAnimation(perceptronAnimation, true)
-//       toggleAnimation(dataAnimWindow, false)
-//     }
-//   })
-//   // .addIndicators({name: "activation"})
-//   // .setPin("#appanananAnimation")
-//   .addTo(controller);
-//
-// let appananaBreakdownScene = new ScrollMagic.Scene({
-//     triggerElement: "#appanana2Text",
-//     duration: '100%'
-//   })
-//   .setTween(graphsTL)
-//   .on("enter", function() {
-//     keepActivation()
-//   })
-//   // .addIndicators({name: "graphs"})
-//   // .setPin("#perceptronAnimation")
-//   .addTo(controller);
-//
-// let applePerceptronSigInScene = new ScrollMagic.Scene({
-//     triggerElement: "#appanana3Text",
-//     duration: '100%'
-//   })
-//   .setTween(sigmoidTL)
-//   .on("enter", function() {
-//     keepActivation()
-//   })
-//   // .setPin("#perceptronAnimation")
-//   .addTo(controller);
-//
-// let applePerceptronSigOutScene = new ScrollMagic.Scene({
-//     triggerElement: "#appanana4Text",
-//     duration: '100%'
-//   })
-//   .setTween(reluTL)
-//   .on("enter", function() {
-//     keepActivation()
-//   })
-//   // .setPin("#perceptronAnimation")
-//   // .addIndicators({name: "sigmoid"})
-//   .addTo(controller);
+let aniScene = new ScrollMagic.Scene({
+    triggerElement: "#text6",
+    duration: '100%'
+  })
+  .setTween(aniTL)
+  .on("start", function(event) {
+    var direction = event.scrollDirection;
+    if (direction == "REVERSE") {
+      toggleAnimation(robotAnimWindow, true)
+      toggleAnimation(agiAnimWindow, false)
+    } else {
+      toggleAnimation(robotAnimWindow, false)
+      toggleAnimation(agiAnimWindow, true)
+    }
+  })
+  .addTo(controller);
+
+let agiScene = new ScrollMagic.Scene({
+    triggerElement: "#text7",
+    duration: '100%'
+  })
+  .setTween(agiTL)
+  .addTo(controller);
+
+
+//-------------------------------------------------
+//----------- DATA ANIMATIONS ---------------
+//-------------------------------------------------
+let data1Scene = new ScrollMagic.Scene({
+    triggerElement: "#text8",
+    duration: '100%'
+  })
+  .setTween(data1TL)
+  .on("start", function(event) {
+    var direction = event.scrollDirection;
+    if (direction == "REVERSE") {
+      toggleAnimation(agiAnimWindow, true)
+      toggleAnimation(dataAnimWindow, false)
+    } else {
+      toggleAnimation(agiAnimWindow, false)
+      toggleAnimation(dataAnimWindow, true)
+    }
+  })
+  .addTo(controller);
+
+let data2Scene = new ScrollMagic.Scene({
+    triggerElement: "#text9",
+    duration: '100%'
+  })
+  .setTween(data2TL)
+  .addTo(controller);
+
+let data3Scene = new ScrollMagic.Scene({
+    triggerElement: "#text10",
+    duration: '100%'
+  })
+  .setTween(data3TL)
+  .addTo(controller);
+
+let data4Scene = new ScrollMagic.Scene({
+    triggerElement: "#text11",
+    duration: '100%'
+  })
+  .setTween(data4TL)
+  .addTo(controller);
+
+
+//-------------------------------------------------
+//----------- WORDS ANIMATIONS ---------------
+//-------------------------------------------------
+let mlScene = new ScrollMagic.Scene({
+    triggerElement: "#text12",
+    duration: '100%'
+  })
+  .setTween(mlTL)
+  .on("start", function(event) {
+    var direction = event.scrollDirection;
+    if (direction == "REVERSE") {
+      toggleAnimation(dataAnimWindow, true)
+      toggleAnimation(wordsAnimWindow, false)
+    } else {
+      toggleAnimation(dataAnimWindow, false)
+      toggleAnimation(wordsAnimWindow, true)
+    }
+  })
+  .addTo(controller);
+
+let dlScene = new ScrollMagic.Scene({
+    triggerElement: "#text13",
+    duration: '100%'
+  })
+  .setTween(dlTL)
+  .addTo(controller);
+
+let algoScene = new ScrollMagic.Scene({
+    triggerElement: "#text14",
+    duration: '100%'
+  })
+  .setTween(algoTL)
+  .addTo(controller);
+
 
 //-------------------------------------------------
 //----------- ENDING SECTION ---------------
 //-------------------------------------------------
 let endingScene = new ScrollMagic.Scene({
     triggerElement: "#endingText",
-    duration: '10%'
-  })
-  .on("enter", function() {
-    offAnimation()
+    duration: '100%'
   })
   .on("start", function(event) {
     var direction = event.scrollDirection;
     if (direction == "REVERSE") {
-      toggleAnimation(dataAnimWindow, true)
+      toggleAnimation(wordsAnimWindow, true)
+    } else {
+      toggleAnimation(wordsAnimWindow, false)
     }
   })
   // .addIndicators({name: "ending"})
